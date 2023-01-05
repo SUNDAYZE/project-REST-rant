99a62c55a18f7e5e043a697ebfb96363af7f57ed
Heriberto Delgado Jr <heribertodelgado92@gmail.com>
2023-01-04 19:53:03 -0800

    Adding 404 page route

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 app.listen(process.env.PORT)
